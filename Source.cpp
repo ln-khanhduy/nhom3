4a36c9b098380752d1b21c1c0f41b439aa0d30cb
Hai <galaxyzed11@gmail.com>
2025-11-04 11:06:42 +0700

    init

--- Source.cpp
+++ Source.cpp
@@ -3,12 +3,20 @@
 using namespace std;
 int tong(int a, int b);
 int hieu(int a, int b);
-int tich(int a, int b);
+int tich(int a, int b){
+	int c;
+	c=a*b;
+	return c;
+};
 float thuong(int a, int b);
+
 int main()
 {
+	int a,b;
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	cout<<"Tong =";
+	int t=tich(a,b);
+	cout<<"Tich 2 so nguyen la: "<<t<<endl;
 	system("pause");
 	return 0;
 }
